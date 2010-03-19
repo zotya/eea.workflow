5cb90b6567c7480bcdc27191e982cbb3f715c10a
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-19 05:27:56 +0000

    Fixes to setup.py; register the skin folder; #3111

--- eea/workflow/__init__.py
+++ eea/workflow/__init__.py
@@ -1,6 +1,20 @@
+from Products.CMFCore import DirectoryView
 from zope.i18nmessageid import MessageFactory
+from eea.workflow.config import *
+
 PortletReadinessMessageFactory = MessageFactory('eea.workflow')
+
+#hack to register the skins path
+from Products.CMFCore import utils
+from Globals import package_home
+from os.path import dirname
+ppath = utils.ProductsPath
+utils.ProductsPath.append(dirname(package_home(product_globals)))
+DirectoryView.registerDirectory('skins', product_globals)
+utils.ProductsPath = ppath
+#endhack
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
-from setuptools import setup, find_packages
 import os
+from os.path import join
+from setuptools import setup, find_packages
 
-version = '0.1'
+name = 'eea.workflow'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
 
-setup(name='eea.workflow',
+setup(name=name,
       version=version,
-      description="Various workflow enhancements for EEA Content",
+      description="EEA Workflow extensions",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      keywords='eea, workflow',
+      author='European Environment Agency',
+      author_email='tiberiu@eaudeweb.ro',
+      url='http://www.eea.europa.eu',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['eea', 'eea.workflow'],
+      namespace_packages=['eea'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
       # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
       """,
       )
