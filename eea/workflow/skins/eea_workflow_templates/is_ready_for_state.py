8cd80de400ed28cef544d717401af2f31f2b9373
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-05-12 17:03:12 +0000

    Delete redundant script

--- eea/workflow/skins/eea_workflow_templates/is_ready_for_state.py
+++
@@ -1,9 +0,0 @@
-##title=Checks if the content item is ready for transition to certain workflow state
-##bind container=container
-##bind context=context
-##bind namespace=
-##bind script=script
-##bind subpath=traverse_subpath
-##parameters=state_name
-
-pass
