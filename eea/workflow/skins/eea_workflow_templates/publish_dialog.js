30f60675afa3595d93a9eeb1e39942813795dac8
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-01-04 16:50:25 +0000

    Refs #7987; Activate Ok button on dialog only when all questions are answered

--- eea/workflow/skins/eea_workflow_templates/publish_dialog.js
+++ eea/workflow/skins/eea_workflow_templates/publish_dialog.js
@@ -20,8 +20,8 @@ function set_publish_dialog(){
             title:"Confirm information before publishing",
             modal:true,
             resizable:true,
-            width:600,
-            height:400,
+            width:700,
+            height:500,
             buttons:{
                 "Ok":function(){
                     var questions_area = $(".questions", target);
@@ -31,8 +31,8 @@ function set_publish_dialog(){
                         if ($(q).hasClass('required')){
                             var radio = $("input[value='yes']", q).get(0);
                             if (radio.checked !== true) {
-                                $("h3", q).after("<div class='notice' style='color:red'>You need to answer with Yes on this question</div>");
-                                $(".notice", q).effect("pulsate", {times:3}, 2000, function(){$('.notice', q).remove()});
+                                $("h3", q).after("<div class='notice' style='color:Black; background-color:Orange; padding:3px'>You need to answer with Yes on this question</div>");
+                                $(".notice", q).effect("pulsate", {times:3}, 2000, function(){$('.notice', q).remove();});
                                 return false;
                             }
                         }
@@ -40,7 +40,7 @@ function set_publish_dialog(){
 
                     var text = make_publish_text(questions_area);
                     $("textarea#comment", target).val(text);
-                    $(questions).remove();
+                    $(".questions").remove();
                     $("form", target).submit();
                     $(this).dialog("close");
                     return false;
@@ -50,11 +50,27 @@ function set_publish_dialog(){
                 }
             },
             open:function(ui){
-                    var base = $("base").attr('href') || document.baseURI || window.location.href.split("?")[0];
-                    var url = base + "/publish_dialog";
-                    $(this).load(url);
-                    return false;
-             }
+
+                     var base = $("base").attr('href') || document.baseURI || window.location.href.split("?")[0];
+                     var url = base + "/publish_dialog";
+                     $(this).load(url, function(){
+                     
+                         //disabling ok button
+                         var okbtn = $('.ui-dialog-buttonpane button:first');
+                         okbtn.attr('disabled', 'disabled').addClass('ui-state-disabled');
+
+                         //see if all radios have a value. When they do, activate the OK button
+                         $(".questions input[type='radio']", target).change(function(){
+                             var questions = $(".question", target);
+                             var activated = $(":radio[checked='true']", target);
+                             if (questions.length === activated.length) {
+                                 okbtn.attr('enabled', 'enabled').removeClass('ui-state-disabled');
+                             }
+                         });
+                     
+                     });
+                     return false;
+                 }
         });
         return false;
     });
