bec6e101956c991cafc2efc06ddb6c8f32ed08a3
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-22 11:48:09 +0000

    Make pylint happy

--- eea/workflow/portlets/readiness.py
+++ eea/workflow/portlets/readiness.py
@@ -1,21 +1,22 @@
+""" Readiness
+"""
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.portlets.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
 from zope.interface import implements
 
 class IReadinessPortlet(IPortletDataProvider):
-    """Readiness portlet
+    """ Readiness portlet
     """
 
 class Assignment(base.Assignment):
-    """Readiness portlet assignment
+    """ Readiness portlet assignment
     """
     implements(IReadinessPortlet)
 
     title = u'Readiness Info'
 
-
 class Renderer(base.Renderer):
-    """Readiness portlet renderer
+    """ Readiness portlet renderer
     """
     render = ViewPageTemplateFile('portlet_readiness.pt')
