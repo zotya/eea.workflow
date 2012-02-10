6ab9c47a854ad96293684d846802038dfd9277e6
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-02-10 11:35:29 +0000

    Improve retrieval of readiness information

--- eea/workflow/portlets/readiness.py
+++ eea/workflow/portlets/readiness.py
@@ -1,6 +1,9 @@
 """ Readiness
 """
+
+from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from eea.workflow.interfaces import IHasMandatoryWorkflowFields
 from plone.app.portlets.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
 from zope.interface import implements
@@ -9,6 +12,7 @@ class IReadinessPortlet(IPortletDataProvider):
     """ Readiness portlet
     """
 
+
 class Assignment(base.Assignment):
     """ Readiness portlet assignment
     """
@@ -16,7 +20,16 @@ class Assignment(base.Assignment):
 
     title = u'Readiness Info'
 
+
 class Renderer(base.Renderer):
     """ Readiness portlet renderer
     """
     render = ViewPageTemplateFile('portlet_readiness.pt')
+
+
+class Readiness(BrowserView):
+    """The @@readiness view
+    """
+
+    def enabled(self):
+        return IHasMandatoryWorkflowFields.providedBy(self.context)
