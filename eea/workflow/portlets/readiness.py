8c11584a79727698d0a47bb679aa54a36962c424
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-07-26 15:13:00 +0000

    Refs #9326; changed the readiness portlet to be a context assignable portlet

--- eea/workflow/portlets/readiness.py
+++ eea/workflow/portlets/readiness.py
@@ -5,8 +5,12 @@ from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from eea.workflow.interfaces import IHasMandatoryWorkflowFields
 from plone.app.portlets.portlets import base
+from plone.app.portlets.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
+from zope.formlib import form
 from zope.interface import implements
+from zope.interface import implements
+
 
 class IReadinessPortlet(IPortletDataProvider):
     """ Readiness portlet
@@ -19,6 +23,15 @@ class Assignment(base.Assignment):
     implements(IReadinessPortlet)
 
     title = u'Readiness Info'
+
+
+class AddForm(base.AddForm):
+    form_fields = form.Fields(IReadinessPortlet)
+    label = u"Add Readiness portlet"
+    description = u"This portlet shows readiness information"
+
+    def create(self, data):
+        return Assignment()
 
 
 class Renderer(base.Renderer):
