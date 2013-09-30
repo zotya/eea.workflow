b21aa8c3309fa78d7d153db7ba1f3cc313537118
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2013-09-30 16:24:30 +0300

    Added viewlet for archive status

--- eea/workflow/browser/viewlets.py
+++ eea/workflow/browser/viewlets.py
@@ -1,2 +1,14 @@
 """ Viewlets
 """
+
+from plone.app.layout.viewlets.common import ViewletBase
+from eea.workflow.interfaces import IObjectArchivator
+
+
+class ArchiveViewlet(ViewletBase):
+    """ Viewlet that appears only when the object is archived
+    """
+
+    def update(self):
+        info = IObjectArchivator(self.context)
+        self.info = info
